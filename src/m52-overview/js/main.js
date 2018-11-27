aaa6fb0526e2a2e45844ef9c0839640ef10fad31
keronos <keronos@voisinville.in>
2018-11-27 15:10:41 +0100

    poursuite de la mise à jour pour le cd93

--- src/m52-overview/js/main.js
+++ src/m52-overview/js/main.js
@@ -6,13 +6,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { connect, Provider } from 'react-redux';
 
-import {hierarchicalM52, hierarchicalAggregated, m52ToAggregated} from '../../shared/js/finance/memoized';
+import { hierarchicalM52, hierarchicalAggregated, m52ToAggregated } from '../../shared/js/finance/memoized';
 import xmlDocumentToDocumentBudgetaire from '../../shared/js/finance/xmlDocumentToDocumentBudgetaire';
 import makeNatureToChapitreFI from '../../shared/js/finance/makeNatureToChapitreFI.js';
 import csvStringToCorrections from '../../shared/js/finance/csvStringToCorrections.js';
 import visitHierarchical from '../../shared/js/finance/visitHierarchical.js';
-import {urls, CORRECTIONS_AGGREGATED} from '../../public/js/constants/resources';
-import {PAR_PUBLIC_VIEW, PAR_PRESTATION_VIEW, M52_INSTRUCTION, AGGREGATED_INSTRUCTION, EXPENDITURES, REVENUE} from '../../shared/js/finance/constants';
+import { urls, CORRECTIONS_AGGREGATED } from '../../public/js/constants/resources';
+import { PAR_PUBLIC_VIEW, PAR_PRESTATION_VIEW, M52_INSTRUCTION, AGGREGATED_INSTRUCTION, EXPENDITURES, REVENUE } from '../../shared/js/finance/constants';
 import {
     DOCUMENT_BUDGETAIRE_RECEIVED, CORRECTION_AGGREGATION_RECEIVED,
 } from '../../public/js/constants/actions';
@@ -21,12 +21,12 @@ import TopLevel from './components/TopLevel.js';
 
 const SOURCE_FINANCE_DIR = './data/finances/'
 
-function reducer(state, action){
-    const {type} = action;
+function reducer(state, action) {
+    const { type } = action;
 
-    switch(type){
+    switch (type) {
         case CORRECTION_AGGREGATION_RECEIVED: {
-            const {corrections} = action;
+            const { corrections } = action;
             return state.set('corrections', corrections);
         }
         case DOCUMENT_BUDGETAIRE_RECEIVED:
@@ -51,7 +51,7 @@ function reducer(state, action){
                 );
         case 'M52_INSTRUCTION_USER_NODE_SELECTED': {
             const { node } = action;
-            const {node: alreadySelectedNode} = state.set('selection') || {};
+            const { node: alreadySelectedNode } = state.set('selection') || {};
 
             return state
                 .set('selection', node && node !== alreadySelectedNode ?
@@ -64,7 +64,7 @@ function reducer(state, action){
         }
         case 'AGGREGATED_INSTRUCTION_USER_NODE_SELECTED': {
             const { node } = action;
-            const {node: alreadySelectedNode} = state.set('selection') || {};
+            const { node: alreadySelectedNode } = state.set('selection') || {};
 
             return state
                 .set('selection', node && node !== alreadySelectedNode ?
@@ -94,17 +94,17 @@ function reducer(state, action){
 
 let childToParent;
 
-function findSelectedNodeAncestors(tree, selectedNode){
-    if(!selectedNode)
+function findSelectedNodeAncestors(tree, selectedNode) {
+    if (!selectedNode)
         return undefined;
 
-    if(!childToParent)
+    if (!childToParent)
         childToParent = new WeakMap();
 
-    if(tree === selectedNode){
+    if (tree === selectedNode) {
         let result = [];
         let current = selectedNode;
-        while(current !== undefined){
+        while (current !== undefined) {
             result.push(current);
             current = childToParent.get(current);
         }
@@ -113,11 +113,11 @@ function findSelectedNodeAncestors(tree, selectedNode){
 
     let ret;
 
-    if(tree.children){
+    if (tree.children) {
         Array.from(tree.children.values()).forEach(child => {
             childToParent.set(child, tree);
             const ancestors = findSelectedNodeAncestors(child, selectedNode);
-            if(ancestors)
+            if (ancestors)
                 ret = ancestors;
         })
     }
@@ -125,12 +125,12 @@ function findSelectedNodeAncestors(tree, selectedNode){
     return ret;
 }
 
-function findSelectedAggregatedNodesByM52Rows(aggregatedNode, m52Rows){
+function findSelectedAggregatedNodesByM52Rows(aggregatedNode, m52Rows) {
     let result = [];
 
     visitHierarchical(aggregatedNode, n => {
         const elements = Array.from(n.elements);
-        if(m52Rows.some(row => elements.some(el => el["M52Rows"].has(row)))){
+        if (m52Rows.some(row => elements.some(el => el["M52Rows"].has(row)))) {
             result.push(n);
         }
     });
@@ -138,11 +138,11 @@ function findSelectedAggregatedNodesByM52Rows(aggregatedNode, m52Rows){
     return new ImmutableSet(result);
 }
 
-function findSelectedM52NodesByM52Rows(M52Node, m52Rows){
+function findSelectedM52NodesByM52Rows(M52Node, m52Rows) {
     let result = [];
 
     visitHierarchical(M52Node, n => {
-        if(m52Rows.some(row => n.elements.has(row))){
+        if (m52Rows.some(row => n.elements.has(row))) {
             result.push(n);
         }
     });
@@ -151,19 +151,19 @@ function findSelectedM52NodesByM52Rows(M52Node, m52Rows){
 }
 
 
-function mapStateToProps(state){
+function mapStateToProps(state) {
     const documentBudgetaire = state.get('documentBudgetaire');
     const corrections = state.get('corrections');
     const rdfi = state.get('RDFI');
     const view = state.get('DF_VIEW');
     const over = state.get('over');
     const selection = state.get('selection');
-    const {type: overType, node: overedNode} = over || {};
-    const {type: selectedType, node: selectedNode} = selection || {};
+    const { type: overType, node: overedNode } = over || {};
+    const { type: selectedType, node: selectedNode } = selection || {};
 
     const expOrRev = rdfi[0] === 'D' ? EXPENDITURES : REVENUE;
 
-    if(!documentBudgetaire)
+    if (!documentBudgetaire)
         return {};
 
     const mainHighlightNode = overedNode || selectedNode;
@@ -177,8 +177,8 @@ function mapStateToProps(state){
     const rdNode = [...aggregatedHierarchical.children].find(c => c.id === expOrRev);
     let rdfiNode = [...rdNode.children].find(c => c.id === rdfi);
 
-    if(rdfi === 'DF'){
-        switch(view){
+    if (rdfi === 'DF') {
+        switch (view) {
             case PAR_PUBLIC_VIEW:
                 // per public is DF-2, so remove DF-1
                 rdfiNode = rdfiNode.removeIn(['children', 0]);
@@ -188,19 +188,19 @@ function mapStateToProps(state){
                 rdfiNode = rdfiNode.removeIn(['children', 1]);
                 break;
             default:
-                throw new Error('Misunderstood view ('+view+')');
+                throw new Error('Misunderstood view (' + view + ')');
         }
     }
 
     let M52HighlightedNodes;
     let aggregatedHighlightedNodes;
 
-    if(mainHighlightType === M52_INSTRUCTION){
+    if (mainHighlightType === M52_INSTRUCTION) {
         M52HighlightedNodes = findSelectedNodeAncestors(M52Hierarchical, mainHighlightNode);
         aggregatedHighlightedNodes = findSelectedAggregatedNodesByM52Rows(rdfiNode, Array.from(mainHighlightNode.elements))
     }
-    else{
-        if(mainHighlightType === AGGREGATED_INSTRUCTION){
+    else {
+        if (mainHighlightType === AGGREGATED_INSTRUCTION) {
             aggregatedHighlightedNodes = findSelectedNodeAncestors(rdfiNode, mainHighlightNode);
             let m52Rows = new ImmutableSet();
             mainHighlightNode.elements.forEach(e => m52Rows = m52Rows.union(e["M52Rows"]));
@@ -218,47 +218,47 @@ function mapStateToProps(state){
     };
 }
 
-function mapDispatchToProps(dispatch){
+function mapDispatchToProps(dispatch) {
     return {
-        onM52NodeOvered(node){
+        onM52NodeOvered(node) {
             dispatch({
                 type: 'M52_INSTRUCTION_USER_NODE_OVERED',
                 node
             });
         },
-        onAggregatedNodeOvered(node){
+        onAggregatedNodeOvered(node) {
             dispatch({
                 type: 'AGGREGATED_INSTRUCTION_USER_NODE_OVERED',
                 node
             });
         },
-        onM52NodeSelected(node){
+        onM52NodeSelected(node) {
             console.log('onM52NodeSelected', node)
             dispatch({
                 type: 'M52_INSTRUCTION_USER_NODE_SELECTED',
                 node
             });
         },
-        onAggregatedNodeSelected(node){
+        onAggregatedNodeSelected(node) {
             dispatch({
                 type: 'AGGREGATED_INSTRUCTION_USER_NODE_SELECTED',
                 node
             });
         },
-        onRDFIChange(rdfi){
+        onRDFIChange(rdfi) {
             dispatch({
                 type: 'RDFI_CHANGE',
                 rdfi
             });
         },
-        onAggregatedDFViewChange(dfView){
+        onAggregatedDFViewChange(dfView) {
             console.log('onAggregatedDFViewChange', dfView);
             dispatch({
                 type: 'DF_VIEW_CHANGE',
                 dfView
             });
         },
-        onNewM52CSVFile(content){
+        onNewM52CSVFile(content) {
             const doc = (new DOMParser()).parseFromString(content, "text/xml");
             natureToChapitreFIP.then(natureToChapitreFI => {
                 dispatch({
@@ -272,8 +272,8 @@ function mapDispatchToProps(dispatch){
 }
 
 const BoundTopLevel = connect(
-  mapStateToProps,
-  mapDispatchToProps
+    mapStateToProps,
+    mapDispatchToProps
 )(TopLevel);
 
 
@@ -301,48 +301,47 @@ const store = createStore(
 
 
 const natureToChapitreFIP = Promise.all([
-    'planDeCompte-2013.xml',
     'planDeCompte-2014.xml',
     'planDeCompte-2015.xml',
     'planDeCompte-2016.xml',
     'planDeCompte-2017.xml'
 ].map(f => fetch(`${SOURCE_FINANCE_DIR}plansDeCompte/${f}`).then(r => r.text())
-    .then( str => {
+    .then(str => {
         return (new DOMParser()).parseFromString(str, "text/xml");
     })
 ))
-.then(makeNatureToChapitreFI)
+    .then(makeNatureToChapitreFI)
 
 
-fetch(`${SOURCE_FINANCE_DIR}CA/CA2016BPAL.xml`).then(resp => resp.text())
-.then(str => {
-    return (new DOMParser()).parseFromString(str, "text/xml");
-})
-.then(doc => {
-    return natureToChapitreFIP.then(natureToChapitreFI => {
-        return xmlDocumentToDocumentBudgetaire(doc, natureToChapitreFI)
+fetch(`${SOURCE_FINANCE_DIR}CA/2017_CA_93.xml`).then(resp => resp.text())
+    .then(str => {
+        return (new DOMParser()).parseFromString(str, "text/xml");
     })
-})
-.then(docBudg => {
-    store.dispatch({
-        type: 'DOCUMENT_BUDGETAIRE_RECEIVED',
-        docBudg,
+    .then(doc => {
+        return natureToChapitreFIP.then(natureToChapitreFI => {
+            return xmlDocumentToDocumentBudgetaire(doc, natureToChapitreFI)
+        })
+    })
+    .then(docBudg => {
+        store.dispatch({
+            type: 'DOCUMENT_BUDGETAIRE_RECEIVED',
+            docBudg,
+        });
     });
-});
 
 fetch(urls[CORRECTIONS_AGGREGATED]).then(resp => resp.text())
-.then(csvStringToCorrections)
-.then(corrections => {
-    store.dispatch({
-        type: CORRECTION_AGGREGATION_RECEIVED,
-        corrections
+    .then(csvStringToCorrections)
+    .then(corrections => {
+        store.dispatch({
+            type: CORRECTION_AGGREGATION_RECEIVED,
+            corrections
+        });
     });
-});
 
 ReactDOM.render(
     React.createElement(
         Provider,
-        {store},
+        { store },
         React.createElement(BoundTopLevel)
     ),
     document.querySelector('.react-container')
